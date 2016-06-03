bd4647e2a33a39b7098d8e14d7a340e862e339bf
Alexsandr Gorbachenko <1111-1111@ukr.net>
2016-06-03 11:16:44 +0300

    change the folder structure

--- test24/users/admin.py
+++ test24/users/admin.py
@@ -1,8 +1,9 @@
+from __future__ import absolute_import
 from django.contrib import admin
 
 # Register your models here.
 
-from users.models import My_users
+from .models import My_users
 
 
 admin.site.register( My_users )
--- test24/users/views.py
+++ test24/users/views.py
@@ -1,3 +1,10 @@
+from __future__ import absolute_import
 from django.shortcuts import render
 
+
 # Create your views here.
+from .models import My_users
+
+def home( request, template=u'base.html' ) :
+    list_user = My_users.objects.all()
+    return render(request,  template, {"list_user": list_user } )
