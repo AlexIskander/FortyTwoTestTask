085e986e3260b20da752a4523f2ae7013953af44
Alexsandr Gorbachenko <1111-1111@ukr.net>
2016-06-03 11:25:25 +0300

    folder test24 delete

--- test24/config/settings.py
+++ test24/config/settings.py
@@ -85,6 +85,14 @@ STATIC_ROOT = join(BASE_DIR, "site_assets")
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
 
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = join(BASE_DIR, "site_media" )
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
 
 
 TEMPLATE_DIRS = (
