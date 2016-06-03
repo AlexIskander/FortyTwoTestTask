7b79c7193ab922ce4df1a13fdd312a726045bb42
Alexsandr Gorbachenko <1111-1111@ukr.net>
2016-06-03 15:39:19 +0300

    modified users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -8,4 +8,10 @@ from .models import ExpansionUsers
 
 # Register your models here.
 
-admin.site.register( ExpansionUsers )
+class ExpansionUsersAdmin(admin.ModelAdmin) :
+    list_display = ["first_name", "last_name",]
+    fields       = ["username", "password", "first_name", "last_name", "email", "image", "biometric_number", "skype", "icq"]
+
+
+
+admin.site.register( ExpansionUsers, ExpansionUsersAdmin )
