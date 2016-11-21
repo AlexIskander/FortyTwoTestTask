bc78691bd1509cf3dc39cf909d9f8625328c08a3
Alexsandr Gorbachenko <1111-1111@ukr.net>
2016-11-21 12:53:54 +0200

    fix to flake8

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -1,99 +1,127 @@
-# -*- coding: utf-8 -*-
-
 """
-Django settings for forty_two_cups_coffy project.
+Django settings for fortytwo_test_task project.
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
 """
-# import python
-import os
-#from os.path import join, abspath, dirname
-from os.path import join, abspath, normpath, dirname
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-here = lambda *dirs: join(abspath(dirname(__file__)), *dirs)
-BASE_DIR = here("..", "..")
-root = lambda *dirs: join(abspath(BASE_DIR), *dirs)
+import os
+import sys
 
+PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
 
+# App/Library Paths
+sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+qpn%7d5yr_s&h2&gv#)#_r1tkf-gr62*fkajdun&7y9u52s2i'
+SECRET_KEY = 'x=c0_e(onjn^80irdy2c221#)2t^qi&6yrc$31i(&ti*_jf3l8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+TEMPLATE_DEBUG = True
+
 ALLOWED_HOSTS = []
 
 
-SITE_ID = 1
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap3',
-    'apps.hello',
-]
 
-MIDDLEWARE_CLASSES = [
-    #'django.middleware.security.SecurityMiddleware',
+    'apps.hello',
+)
+
+MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+)
 
 ROOT_URLCONF = 'fortytwo_test_task.urls'
-
-
 
 WSGI_APPLICATION = 'fortytwo_test_task.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': root('db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-
-
 # Internationalization
-# https://docs.djangoproject.com/en/1.9/topics/i18n/
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
-TIME_ZONE = 'Europe/Kiev'
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
+# Upload Media
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'uploads')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/uploads/'
 
 
-
-MEDIA_ROOT = root( "site_media" )
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
-
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = root( "site_assets" )
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
 STATIC_URL = '/static/'
 
-
-TEMPLATE_DIRS = (
-    root( 'templates' ),
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'assets'),
 )
+
+
+# Template Settings
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or
+    # "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+# Turn off south during test
+SOUTH_TESTS_MIGRATE = False
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,18 +1,14 @@
-"""temp URL Configuration
-"""
-# import django
-from django.conf import settings
-from django.conf.urls import url, patterns
+
+from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = patterns('',
-    #url(r'^$', "apps.users.views.home" ),
-    url(r'^admin/', admin.site.urls),
+urlpatterns = patterns(
+    '',
+    # Examples:
+    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
 )
-
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$',  'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, } ),
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, } ),
-    )
