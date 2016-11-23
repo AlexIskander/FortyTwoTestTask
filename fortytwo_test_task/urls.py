33029c0773afd542322b6ba0bdbb2c5bd3f6e8ed
Alexsandr Gorbachenko <1111-1111@ukr.net>
2016-11-23 12:55:40 +0200

    ticket#1 add modified urls.py

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,14 +1,29 @@
-
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from fortytwo_test_task import settings
+
 admin.autodiscover()
+
 
 urlpatterns = patterns(
     '',
     # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
+    url(r'^$', 'apps.contact.views.home_page', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# If this is in DEBUG/runserver mode, did you remember to add to your urls.py
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            url(r'^uploads/(?P<path>.*)$',
+                                'django.views.static.serve',
+                                {'document_root': settings.MEDIA_ROOT,
+                                 }),
+                            url(r'^static/(?P<path>.*)$',
+                                'django.views.static.serve',
+                                {'document_root': settings.STATIC_ROOT,
+                                 }),
+                            )
