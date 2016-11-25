f7fcff3d9159c066a639bb67af94a95d20ccf7ad
Alexsandr Gorbachenko <1111-1111@ukr.net>
2016-11-25 15:25:19 +0200

    ticket 1 modified views.py (delete pagination)

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,23 +1,13 @@
 from __future__ import absolute_import
 
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 from .models import ExpansionUsers
 # Create your views here.
 
 
-def home_page(request, template="base.html", output_number_page=10):
+def home_page(request, template="base.html", limit=1):
 
-    list_user = ExpansionUsers.objects.all()
-    # Show  users per page
-    paginator = Paginator(list_user, output_number_page)
-
-    page = request.GET.get('page')
-    try:
-        users = paginator.page(page)
-    except PageNotAnInteger:
-        users = paginator.page(1)
-    except EmptyPage:
-        users = paginator.page(paginator.num_pages)
+    users = ExpansionUsers.objects.all()[:limit]
     return render(request, template, {"users": users})
